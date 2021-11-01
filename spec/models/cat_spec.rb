2ecb5eb32d9631e42e0415137edf1e642db38323
JBrady <jbradysd@icloud.com>
2021-11-01 16:46:10 -0700

    validation

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
+    it 'should have a valid name' do 
+        cat = Cat.create(age: 3, enjoys: 'all the attention')
+        expect(cat.errors[:name]).to_not be_empty
+    end
+
+    it 'should have a valid age' do 
+        cat = Cat.create(name: 'Toast', enjoys: 'all the attention')
+        expect(cat.errors[:age]).to_not be_empty
+    end
+
+    it 'should have a valid enjoys' do 
+        cat = Cat.create(name: 'Toast', age: 2)
+        expect(cat.errors[:enjoys]).to_not be_empty
+    end
+
+    it 'should have an enjoys value of at least 10 characters' do
+        cat = Cat.create(name: 'Toast', age: 2, enjoys: 'eating')
+        expect(cat.errors[:enjoys]).to_not be_empty
+    end
 end
